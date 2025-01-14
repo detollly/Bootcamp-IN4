9d5b19315fa858d9b854c9d82ad62c41e9dd1e23
detollly <anatoly.demianov@gmail.com>
2025-01-14 11:47:27 +0000

    init class

---
+++ OOP/student.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+        
+def __str__(self):
+    return f"Student Name: {self.name}"
+    
+new_Student = Student('Calum', 20, 'A')
+
+print(new_Student)
+    
+    
