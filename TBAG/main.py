7558b689d18c6287771aa56c5c8ef7040758ff41
detollly <anatoly.demianov@gmail.com>
2025-01-17 12:02:58 +0000

    set everything up to step 20 inc. With bugs

--- TBAG/main.py
+++ TBAG/main.py
@@ -1,7 +1,21 @@
 from room import Room
 
-kitchen = Room("kitchen")
-ballroom = Room("ballroom")
-dining_hall = Room("dining_hall")
-
+kitchen = Room()
+kitchen.set_name("kitchen")
 kitchen.set_description("A dark and dirty room buzzing with flies")
+
+ballroom = Room()
+ballroom.set_name("ballroom")
+ballroom.set_description("A vast room with a shining wooden floor")
+
+dining_hall = Room()
+dining_hall.set_name("dining_hall")
+dining_hall.set_description("A large room with ornate golden decorations")
+
+# directions
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom,"west")
+ballroom.link_room(dining_hall,"east")
+
+dining_hall.get_details()
--- TBAG/room.py
+++ TBAG/room.py
@@ -1,9 +1,10 @@
 class Room:
     
     # constructor    
-    def __init__(self, room_name):
-        self.name = room_name
+    def __init__(self):
+        self.name = None
         self.description = None
+        self.linked_rooms = {}
         
         
     # creating getter and setter
@@ -13,4 +14,25 @@ class Room:
     def set_description(self, room_description):
         self.description = room_description
         
+    def describe(self):
+        print(self.description)
+        
+    def get_name(self):
+        return self.name
+    
+    def set_name(self, room_name):
+        self.name = room_name  
+        
+    # linking rooms and navigating them
+        
+    def link_room(self, room_to_link, direction):
+        self.linked_room[direction] = room_to_link
+        
+    def get_details(self):
+        print(self.name)
+        print("------------")
+        print(self.description)
+        for direction in self.linked_room:
+            room = self.linked_rooms[direction]
+        print("The " + room.get_name()+ " is " + direction)
     
